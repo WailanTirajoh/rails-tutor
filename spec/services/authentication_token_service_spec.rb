73d25414f307ee10ca5ec2c3f6d65b6f92e3b763
Wailan Tirajoh <wailantirajoh@gmail.com>
2023-09-28 17:51:22 +0700

    feat: user authentication (jwt)

--- spec/services/authentication_token_service_spec.rb
+++ spec/services/authentication_token_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe AuthenticationTokenService do
   describe '.call' do
     it 'returns an authentication token' do
-      token = described_class.call
+      token = described_class.call(1)
       decoded_token = JWT.decode(
         token,
         described_class::HMAC_SECRET,
@@ -13,8 +13,8 @@ describe AuthenticationTokenService do
 
       expect(decoded_token).to eq(
         [
-          { 'test' => 'blah' }, # payload
-          { 'alg' => 'HS256' } # header
+          { 'user_id' => 1 },
+          { 'alg' => 'HS256' }
         ]
       )
     end
